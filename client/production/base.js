fc0c83144230ecc45dc8523c0310686b58dea2e1
AminMusah <ameenmusah77@gmail.com>
2023-03-24 15:37:39 -0500

    change production link

--- client/production/base.js
+++ client/production/base.js
@@ -1,3 +1,4 @@
-const production = 'https://space-x-lemon.vercel.app/'
+// const production = 'https://space-x-lemon.vercel.app/'
+const production = 'https://space-x-lemon.vercel.app'
 
 export default production;
--- server/api/server.js
+++ server/api/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const launches = require('../routes/launches');
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
 //send json data 
-app.use(express.json())
+app.use(express.json());
 
 //routes
-app.use('/api/',launches)
-// app.use('/api/',launches)
+app.use('/api/',launches);
 
-const PORT =  process.env.PORT || 5000
+const PORT =  process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`listening on port ${PORT}`))
+app.listen(PORT, console.log(`listening on port ${PORT}`));
